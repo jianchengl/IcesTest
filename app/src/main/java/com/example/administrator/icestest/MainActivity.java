d02372cf453d76e479ffd6b13edfb7d1507692c5
Adde <liujiancheng811@163.com>
2018-03-09 11:37:41 +0800

    dev

--- app/src/main/java/com/example/administrator/icestest/MainActivity.java
+++ app/src/main/java/com/example/administrator/icestest/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        //test
+        //dev
     }
 
     @Override
